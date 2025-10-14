b7ce02bfd025a580e2074e66a53f605cf41358aa
Jesús Perera <jesus2perera3@gmail.com>
2025-10-13 21:58:42 -0400

    feat: implementar vista para el modelo Book utilizando ViewSet de Django REST Framework

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint que permite a los libros ser vistos o editados.
+    """
+    queryset = Book.objects.all().order_by('id')
+    serializer_class = BookSerializer
