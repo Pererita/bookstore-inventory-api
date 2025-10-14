da036abc83b4d27f5731bad8c7f47cb07ea5cb22
Jesús Perera <jesus2perera3@gmail.com>
2025-10-13 23:25:21 -0400

    feat: agregar soporte para drf-spectacular y configurar URLs de documentación OpenAPI

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'inventory',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -131,5 +132,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'EXCEPTION_HANDLER': 'inventory.exceptions.custom_exception_handler'
 }
--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/api/v1/books/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/v1/', include('inventory.urls')),
+
+    # URLs de la Documentación (OpenAPI)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
